e693d3fc7e244ac198aa0a6b763595340fc72962
Gabriel Coelho <gabriellvs60@gmail.com>
2019-08-05 19:05:47 -0300

    Criando usuário no banco
    Criando usuário no banco através do NodeJs
    NodeJs + MongoDB + Mongoose

--- aprendendo.js
+++ aprendendo.js
@@ -8,3 +8,52 @@ mongoose.connect('mongodb://localhost:27017/students', { useNewUrlParser: true }
 
 //para criar um banco no mongo (terminal)
 //use banco_loja, assim ele cria automaticamente pra você.
+
+//Model - Usuários
+//DEFININDO O MODEL
+
+//'Schema' é um padrão do mongoose
+const UsuariosSchema = mongoose.Schema({
+//String number date object
+    nome:{
+        type: String,
+        //require especifica se é obrigatório (true ou false)
+        require: true
+    },
+    sobrenome:{
+        type: String,
+        require: true
+    },
+    email: {
+        type: String,
+        require: true
+    },
+    idade:{
+        type: Number,
+        require: true
+    },
+    pais:{
+        type: String
+    }
+})
+//Collection
+//não existem tabelas no mongo, existem collections
+mongoose.model('usuarios', UsuariosSchema)
+
+const novoUsuario = mongoose.model('usuarios')
+new novoUsuario({
+    nome: "Victor",
+    email: "email@terra",
+    idade: 19,
+    pais: "Brasil"
+}).save().then(() => {
+    console.log("Usuario criado com sucesso")
+}).catch(() => {
+    console.log("Houve um erro ao registrar o usuario: " + err)
+})
+
+//mongo
+//show databases;
+//use students;
+//show collections;
+//db.usuarios.find();
